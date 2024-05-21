276e8d40d71953d745d6b73e27fcb973bc0d82d4
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-05-21 15:53:07 -0500

    add support for generating swagger api docs

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sashabaranov/go-fastapi"
+	"fmt"
+	"encoding/json"
+	"os"
+)
+
+type EchoInput struct {
+	Phrase string `json:"phrase"`
+}
+
+type EchoOutput struct {
+	OriginalInput EchoInput `json:"original_input"`
+}
+
+func EchoHandler(ctx *gin.Context, in EchoInput) (out EchoOutput, err error) {
+	out.OriginalInput = in
+	return
+}
+
+func generateSwagger(fn string, router *fastapi.Router) error {
+	swagger := router.EmitOpenAPIDefinition()
+	swagger.Info.Title = "Ranked Choice Voting API"
+	
+	f, err := os.Create(fn)
+	if err != nil {
+		return err
+	}
+	
+	defer f.Close()
+
+	jsonBytes, _ := json.MarshalIndent(swagger, "", "    ")
+	_, err = f.Write(jsonBytes)
+	if err != nil {
+		return err
+	}
+	
+	fmt.Printf("Wrote Swagger to %s\n", fn)
+
+	return nil
+
+}
+
+func main() {
+	r := gin.Default()
+
+	router := fastapi.NewRouter()
+	router.AddCall("/echo", EchoHandler)
+
+	generateSwagger("swagger.json", router)
+
+	r.POST("/api/*path", router.GinHandler) // must have *path parameter
+	r.Run()
+}
+
+// Try it:
+//     $ curl -H "Content-Type: application/json" -X POST --data '{"phrase": "hello"}' localhost:8080/api/echo
+//     {"response":{"original_input":{"phrase":"hello"}}}
